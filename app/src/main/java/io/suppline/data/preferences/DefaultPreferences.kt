363f686ed876042365bb6390f3da0b26d46cc638
MatLeg <mat.legierskii@gmail.com>
2024-08-31 20:07:29 +0200

    move  getForToday to GetDailySupplementationUseCase

--- app/src/main/java/io/suppline/data/preferences/DefaultPreferences.kt
+++ app/src/main/java/io/suppline/data/preferences/DefaultPreferences.kt
@@ -1,4 +1,5 @@
 package io.suppline.data.preferences
+
 import android.content.SharedPreferences
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
@@ -6,12 +7,10 @@ import io.suppline.domain.models.DailySupplementation
 import io.suppline.domain.preferences.Preferences
 import io.suppline.domain.utils.serializer.DailySupplementationDeserializer
 import io.suppline.domain.utils.serializer.DailySupplementationSerializer
-import java.time.LocalDate
-
 
 class DefaultPreferences(
     private val sharedPref: SharedPreferences
-): Preferences {
+) : Preferences {
 
     private val gson: Gson = GsonBuilder()
         .registerTypeAdapter(DailySupplementation::class.java, DailySupplementationSerializer())
@@ -36,26 +35,13 @@ class DefaultPreferences(
     override fun loadDailySupplements(): DailySupplementation? {
         val supplementationJson = sharedPref.getString(Preferences.KEY_DAILY_SUPPLEMENTS, null)
         return if (supplementationJson != null) {
-            val supplementation: DailySupplementation? = gson.fromJson(supplementationJson, DailySupplementation::class.java)
-            supplementation?.let {
-                getForToday(it)
-            }
+            val supplementation: DailySupplementation? =
+                gson.fromJson(supplementationJson, DailySupplementation::class.java)
+            supplementation
         } else {
             null
         }
     }
 
-    private fun getForToday(supplementation: DailySupplementation): DailySupplementation {
-        val today = LocalDate.now()
-        val isToday = supplementation.date.isEqual(today)
-        return if (isToday) supplementation else {
-            supplementation.copy(
-                date = today,
-                supplements = supplementation.supplements.map { supplement ->
-                    supplement.copy(consumed = false)
-                }
-            )
-        }
-    }
 
 }
--- app/src/main/java/io/suppline/domain/useCase/GetDailySupplementationUseCase.kt
+++ app/src/main/java/io/suppline/domain/useCase/GetDailySupplementationUseCase.kt
@@ -10,21 +10,40 @@ import java.time.LocalTime
 class GetDailySupplementationUseCase(
     private val preferences: Preferences
 ) {
+
     operator fun invoke(): DailySupplementation {
-        return preferences.loadDailySupplements() ?: kotlin.run {
-            DailySupplementation(
-                date = LocalDate.now(),
-                supplements = Config.DEFAULT_SUPPLEMENT_NAMES.mapIndexed { index, s ->
-                    Supplement(
-                        id = index,
-                        name = s,
-                        consumed = false,
-                        timeToConsume = LocalTime.now(),
-                        hasNotification = false
-                    )
+        val savedSupplementation = preferences.loadDailySupplements()
+        return if (savedSupplementation == null) getDefaultDailySupplementation()
+        else getForToday(savedSupplementation)
+    }
+
+    private fun getForToday(supplementation: DailySupplementation): DailySupplementation {
+        val today = LocalDate.now()
+        val isToday = supplementation.date.isEqual(today)
+        return if (isToday) supplementation else {
+            supplementation.copy(
+                date = today,
+                supplements = supplementation.supplements.map { supplement ->
+                    supplement.copy(consumed = false)
                 }
             )
         }
     }
+
+    private fun getDefaultDailySupplementation(): DailySupplementation {
+        return DailySupplementation(
+            date = LocalDate.now(),
+            supplements = Config.DEFAULT_SUPPLEMENT_NAMES.mapIndexed { index, s ->
+                Supplement(
+                    id = index,
+                    name = s,
+                    consumed = false,
+                    timeToConsume = LocalTime.now(),
+                    hasNotification = false
+                )
+            }
+        )
+    }
+
 }
 
