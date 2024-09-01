1fcc5a1d76395529f451302251ad9afd2ea4df76
MatLeg <mat.legierskii@gmail.com>
2024-09-01 17:14:31 +0200

    todo update test

--- app/src/test/java/io/suppline/presentation/SuppLineViewModelTest.kt
+++ app/src/test/java/io/suppline/presentation/SuppLineViewModelTest.kt
@@ -1,10 +1,19 @@
 package io.suppline.presentation
 
+import io.suppline.data.preferences.DefaultPreferences
 import io.suppline.domain.preferences.Preferences
+import org.junit.Before
 
+//todo update test
 class SuppLineViewModelTest() {
 
     lateinit var preferences: Preferences
     lateinit var viewModel: SuppLineViewModel
+
+    @Before
+    fun setup() {
+//        preferences = DefaultPreferences()
+//        viewModel = SuppLineViewModel()
+    }
 
 }
