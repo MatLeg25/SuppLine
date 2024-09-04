e127e813e67a24b73a429bf3f2aac01b41554f2c
MatLeg <mat.legierskii@gmail.com>
2024-09-04 17:40:52 +0200

    add mockk for tests

--- app/src/test/java/io/suppline/presentation/SuppLineViewModelTest.kt
+++ app/src/test/java/io/suppline/presentation/SuppLineViewModelTest.kt
@@ -1,8 +1,14 @@
 package io.suppline.presentation
 
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
 import io.suppline.data.preferences.DefaultPreferences
+import io.suppline.domain.models.Supplement
 import io.suppline.domain.preferences.Preferences
+import io.suppline.presentation.states.SuppLineState
 import org.junit.Before
+import java.time.LocalTime
 
 //todo update test
 class SuppLineViewModelTest() {
@@ -10,10 +16,34 @@ class SuppLineViewModelTest() {
     lateinit var preferences: Preferences
     lateinit var viewModel: SuppLineViewModel
 
+    private val DEFAULT_SUPPLEMENTS = listOf(
+        Supplement(
+            id = 1,
+            name = "One",
+            consumed = false,
+            timeToConsume = LocalTime.of(11,0),
+            hasNotification = false
+        ),
+        Supplement(
+            id = 2,
+            name = "Two",
+            consumed = false,
+            timeToConsume = LocalTime.of(12,0),
+            hasNotification = false
+        ),
+        Supplement(
+            id = 3,
+            name = "Three",
+            consumed = false,
+            timeToConsume = LocalTime.of(13,0),
+            hasNotification = false
+        )
+    )
+
     @Before
     fun setup() {
-//        preferences = DefaultPreferences()
-//        viewModel = SuppLineViewModel()
+        viewModel = mockk<SuppLineViewModel>()
+        every { viewModel.state.value } returns SuppLineState(supplements = DEFAULT_SUPPLEMENTS)
     }
 
 }
