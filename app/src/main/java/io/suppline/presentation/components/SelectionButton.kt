2ed611dab6d59a941c06c0182525460925d1e91d
MatLeg <mat.legierskii@gmail.com>
2024-11-05 17:57:12 +0100

    update SelectionButton UI

--- app/src/main/java/io/suppline/presentation/components/SelectionButton.kt
+++ app/src/main/java/io/suppline/presentation/components/SelectionButton.kt
@@ -4,20 +4,19 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Done
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import io.suppline.R
 
 @Composable
@@ -34,6 +33,7 @@ fun SelectionButton(
 
     Button(
         modifier = modifier,
+        contentPadding = PaddingValues(),
         colors = ButtonColors(
             containerColor = animatedButtonColor.value,
             contentColor = animatedButtonColor.value,
@@ -41,15 +41,19 @@ fun SelectionButton(
             disabledContentColor = animatedButtonColor.value,
         ),
         onClick = { onClick() }) {
-
         AnimatedVisibility(visible = isSelected) {
             Icon(
+                modifier = modifier,
                 imageVector = Icons.Default.Done,
                 contentDescription = stringResource(id = R.string.app_name),
                 tint = Color.Blue
             )
         }
-
     }
+}
 
+@Composable
+@Preview
+fun SelectionButtonPreview() {
+    SelectionButton(modifier = Modifier.size(50.dp), isSelected = true, onClick = { })
 }
