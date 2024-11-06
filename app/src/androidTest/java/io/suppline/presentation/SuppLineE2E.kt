648d291f06adf04e2985f0156e347aa98905187c
MatLeg <mat.legierskii@gmail.com>
2024-11-06 15:42:38 +0100

    add tests in progress, update add_and_delete_items_correctly_update_ui_and_preferences

--- app/src/androidTest/java/io/suppline/presentation/SuppLineE2E.kt
+++ app/src/androidTest/java/io/suppline/presentation/SuppLineE2E.kt
@@ -1,30 +1,31 @@
 package io.suppline.presentation
 
 
-import androidx.activity.compose.setContent
 import androidx.annotation.StringRes
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.CheckCircle
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.semantics.SemanticsProperties
 import androidx.compose.ui.test.SemanticsMatcher
 import androidx.compose.ui.test.isDisplayed
+import androidx.compose.ui.test.isNotDisplayed
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performTextInput
 import io.suppline.R
 import io.suppline.data.FakePreferences
+import io.suppline.domain.models.DailySupplementation
 import io.suppline.domain.useCase.GetDailySupplementationUseCase
 import io.suppline.domain.useCase.SaveDailySupplementationUseCase
-import io.suppline.presentation.components.ActionButton
-import io.suppline.presentation.ui.theme.SuppLineTheme
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import java.time.LocalDate
 
 class SuppLineE2E {
+
+    private val sampleName = "Sample name"
+    private val sampleDescription = "description"
 
     @get:Rule
     val composeRule = createAndroidComposeRule<MainActivity>()
@@ -40,6 +41,7 @@ class SuppLineE2E {
     @Before
     fun setUp() {
         preferencesFake = FakePreferences()
+        preferencesFake.saveDailySupplementation(DailySupplementation(LocalDate.now(), emptyList()))
         viewModelFake = SuppLineViewModel(
             getSupplementation = GetDailySupplementationUseCase(preferencesFake),
             saveSupplementation = SaveDailySupplementationUseCase(preferencesFake),
@@ -47,7 +49,8 @@ class SuppLineE2E {
     }
 
     @Test
-    fun firstTest() {
+    fun add_and_delete_items_correctly_update_ui_and_preferences() {
+        assert(viewModelFake.state.value.supplements.isEmpty())
         //top logo isDisplayed
         composeRule.onNodeWithText(text = getString(R.string.suppline_title)).isDisplayed()
         //check AddEdit modal
@@ -55,7 +58,18 @@ class SuppLineE2E {
         composeRule.onNodeWithTag(getString(R.string.add)).isDisplayed()
         composeRule.onNodeWithTag(getString(R.string.cancel)).isDisplayed()
         composeRule.onNodeWithTag(getString(R.string.remove)).isDisplayed()
-        //
+
+        //prevent adding item with empty name
+        composeRule.onNodeWithText(getString(R.string.name)).performTextInput("")
+        composeRule.onNodeWithTag(getString(R.string.add)).performClick()
+        composeRule.onNodeWithTag(getString(R.string.add)).isDisplayed()
+
+        //add new item with [sampleName]
+        composeRule.onNodeWithText(getString(R.string.name)).performTextInput(sampleName)
+        composeRule.onNodeWithTag(getString(R.string.add)).performClick()
+        //close AddEdit item modal
+        composeRule.onNodeWithTag(getString(R.string.add)).isNotDisplayed()
+
         assert(1 == 1)
     }
 
--- app/src/main/java/io/suppline/domain/Config.kt
+++ app/src/main/java/io/suppline/domain/Config.kt
@@ -15,7 +15,7 @@ object Config {
 
     const val SHARED_PREFERENCES_NAME = "SuppLine_shared_preferences"
 
-    const val MAX_DESCRIPTION_LENGTH = 10
+    const val MAX_DESCRIPTION_LENGTH = 100
     const val MAX_NEW_LINES = 3
 
 }
